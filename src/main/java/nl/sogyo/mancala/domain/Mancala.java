f3530dc0d428c48c27d78fc96342e020cf793f08
Rik van der Stelt <rvdstelt@sogyo.nl>
2020-08-19 17:37:27 +0200

    Add players and simple methods on them

--- src/main/java/nl/sogyo/mancala/domain/Mancala.java
+++ src/main/java/nl/sogyo/mancala/domain/Mancala.java
@@ -1,16 +1,20 @@
 package nl.sogyo.mancala.domain;
 
+import sun.jvm.hotspot.utilities.Assert;
+
 abstract class Container{
     private int numberOfBeads;
     private Container nextContainer;
+    private Player owner;
 
     public Container(){
         numberOfBeads = 0;
     }
 
-    public Container(Container nextContainer){
+    public Container(Container nextContainer, Player owner){
         numberOfBeads = 0;
         this.nextContainer = nextContainer;
+        this.owner = owner;
     }
 
     public void addBead(){
@@ -28,6 +32,10 @@ abstract class Container{
     public Container getNextContainer() {
         return nextContainer;
     }
+
+    public boolean isOwnersTurn() {
+        return owner.isMyTurn();
+    }
 }
 
 class Pit extends Container {
@@ -35,12 +43,43 @@ class Pit extends Container {
         super();
         this.addBead(4);
     }
-    public Pit(Container nextContainer){
-        super(nextContainer);
+    public Pit(Container nextContainer,Player owner){
+        super(nextContainer,owner);
         this.addBead(4);
     }
 }
 
 class Kalaha extends Container{
 
+}
+
+
+class Player {
+    private boolean myTurn;
+    private Player opponent;
+
+    public Player(boolean myTurn){
+        this.myTurn = myTurn;
+    }
+
+    public boolean isMyTurn() {
+        return myTurn;
+    }
+
+    public void makeOpponents(Player player2){
+        if(myTurn != player2.myTurn){
+            player2.opponent = this;
+            this.opponent = player2;
+        } else {
+            System.out.println("Error: two players in same turn state");
+        }
+    }
+
+    public void flipOpponent(){
+        opponent.flipSelf();
+    }
+
+    public void flipSelf() {
+        myTurn = !myTurn;
+    }
 }
--- src/test/java/nl/sogyo/mancala/domain/MancalaTest.java
+++ src/test/java/nl/sogyo/mancala/domain/MancalaTest.java
@@ -16,8 +16,38 @@ public class MancalaTest {
 
     @Test
     public void testGetNextPit(){
+        Player player1 = new Player(true);
         Pit pit2 = new Pit();
-        Pit pit1 = new Pit(pit2);
+        Pit pit1 = new Pit(pit2,player1);
         Assert.assertEquals("A neighbouring pit must be able to be found ", pit1.getNextContainer(),pit2);
+    }
+
+    @Test
+    public void testIsOwnersTurn(){
+        Pit pit2 = new Pit();
+        Player player1 = new Player(true);
+        Pit pit1 = new Pit(pit2,player1);
+
+        Assert.assertTrue(pit1.isOwnersTurn());
+    }
+
+    @Test
+    public void testFlipSelf() {
+        Player player1 = new Player(true);
+
+        player1.flipSelf();
+
+        Assert.assertFalse(player1.isMyTurn());
+    }
+
+    @Test
+    public void testFlipOpponent() {
+        Player player1 = new Player(true);
+        Player player2 = new Player(false);
+        player1.makeOpponents(player2);
+
+        player2.flipOpponent();
+
+        Assert.assertFalse(player1.isMyTurn());
     }
 }
